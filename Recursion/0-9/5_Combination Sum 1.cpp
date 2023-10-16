614b4a627e0920b36c515cb6edc408bf2a230dde
mritunjayRoy <mroy8202@gmail.com>
2023-10-17 00:20:11 +0530

    Combinational Sum 1

---
+++ Recursion/0-9/5_Combination Sum 1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    void combinations(int ind, int target, vector<int> ds, vector<vector<int>>& ans, vector<int>& candidates) {
+        // base case
+        if(ind >= candidates.size()) {
+            if(target == 0) ans.push_back(ds);
+            return ;
+        }
+
+        // pick element
+        if(candidates[ind] <= target) {
+            ds.push_back(candidates[ind]);
+            combinations(ind, target-candidates[ind], ds, ans, candidates);
+            ds.pop_back();
+        }
+
+        // donot pick element
+        combinations(ind+1, target, ds, ans, candidates);
+    }
+
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        combinations(0, target, ds, ans, candidates);
+        return ans;
+    }
+};
